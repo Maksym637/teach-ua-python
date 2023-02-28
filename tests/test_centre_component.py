1841a6624797121e3530d5b995d4f4c38137340b
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2023-03-01 01:23:27 +0200

    Implementation of TUA-160

--- tests/test_centre_component.py
+++ tests/test_centre_component.py
@@ -13,7 +13,7 @@ class TestCentreComponent(LoginRunner):
         ("location-2.2", 2, 2, "street 2.2", "30.4485253, 50.4735083", "2424242424", "location-2.2"),
         ("test-location", 4, 4, "test 12, 12", "25.4485253, 25.4735083", "0681245611", "test-location")
     ])
-    def test_add_location(self, location_name, city_number, region_number, 
+    def test_add_location_valid(self, location_name, city_number, region_number, 
                           address, coordinates, phone, expected_new_location):
         home_page = HomePage(self.driver)
         centre_main_info = CentreMainInfoComponent(self.driver)
@@ -33,3 +33,21 @@ class TestCentreComponent(LoginRunner):
         actual_new_location = centre_main_info.get_name_new_location()
 
         self.assertEqual(actual_new_location, expected_new_location)
+    
+    """Implementation of TUA-160
+    """
+    def test_add_location_invalid(self):
+        home_page = HomePage(self.driver)
+
+        actual_error_msg = home_page.header \
+            .move_to_user_menu() \
+            .click_add_centre() \
+            .click_add_location() \
+            .click_add_button() \
+            .get_error_messages()
+        
+        self.assertListEqual(
+            actual_error_msg, ["Некоректна назва локації", "Це поле є обов'язковим",
+                               "Це поле є обов'язковим", "Некоректна адреса",
+                               "Некоректні координати", "Це поле є обов'язковим"]
+        )
